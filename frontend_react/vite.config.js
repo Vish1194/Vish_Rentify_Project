89db81aaa2d70bdb0082cb5f5eda3a3f2ce110b5
Vish1194 <152699085+Vish1194@users.noreply.github.com>
2024-05-27 21:26:46 +0530

    update vite config.js

--- frontend_react/vite.config.js
+++ frontend_react/vite.config.js
@@ -10,6 +10,7 @@ const targetApiUrl = 'https://vish-rentify-project.onrender.com';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react()],
   server:{
     port: FRONTEND_PORT,
