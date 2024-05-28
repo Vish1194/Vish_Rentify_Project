d2e52e0af49511110801e89aa065f6a84418042b
Vish1194 <152699085+Vish1194@users.noreply.github.com>
2024-05-28 14:55:12 +0530

    rolling added

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,7 @@ const app = express();
 app.use(session(
     {
         secret:"SomeSecretCode",
+        rolling:true,
         cookie:{maxAge:1000*60*60 , httpOnly:true},
         resave:false,
         saveUninitialized:false
